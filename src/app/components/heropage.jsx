81fb99f7d6bd62f8529990ea0b3955876f6d8122
Ekele1 <jerryoilfarmltd01@gmail.com>
2024-08-21 04:08:14 +0100

    rr

--- src/app/components/heropage.jsx
+++ src/app/components/heropage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Heropage = () => {
   return (
-    <div className='w-full bg-[url("/homepage.jpg")] lg:bg-auto bg-cover h-screen lg:pt-20 lg:pl-20 p-5 leading-[50px] mt-10'>
+    <div className='w-full bg-[url("/homepage.jpg")] lg:bg-auto bg-cover bg-center h-screen lg:pt-20 lg:pl-20 p-5 leading-[50px] mt-10'>
         <h1 className='font-bold lg:text-[50px] text-[35px] text-white'>Take control of your finances</h1>
         <h1 className='font-bold lg:text-[50px] text-[35px] text-white'>with expense manager</h1>
         <p className='text-[20px] text-white typewriter-text'>Effortlessly track your expenses, manage budgets, and achieve your financial goals.</p>
