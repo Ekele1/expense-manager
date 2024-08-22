d272352eff48d510c3f907e3a46728d87fd82170
Ekele1 <jerryoilfarmltd01@gmail.com>
2024-08-22 22:11:45 +0100

    rr

--- src/app/aboutUs/page.jsx
+++ src/app/aboutUs/page.jsx
@@ -1,9 +1,47 @@
+import Image from 'next/image'
 import React from 'react'
 
 const About = () => {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      About us comming soon
+    <div className="w-full">
+      <div className='w-full bg-[url(/about.jpg)] bg-cover h-screen flex justify-center pt-[100px]'>
+        <div className='lg:w-[50%] w-full h-max bg-white/10 backdrop-blur-md p-5 lg:p-10 rounded-md'>
+          <p className='lg:text-[25px] text[20px] font-bold text-orange-800'>
+          We believe that financial freedom starts with smart money management. Our mission is to
+           empower individuals and businesses to take control of their finances, simplify their 
+           budgeting, and achieve their financial goals with confidence.
+          </p>
+        </div>
+      </div>
+      <div className='w-full pt-20 pl-4 pr-4'>
+        <div className='text-[30px] font-bold text-[#062134]'>
+          <p>Our vision is to create a world where everyone has the tools and knowledge they need to manage their finances effectively.</p>
+        </div>
+        <div className='w-full flex lg:flex-row flex-col gap-10 pt-10'>
+          <div className='lg:w-[50%] w-full text-[18px] text-[#062134] font-semibold'>
+            <p>
+            Whether you're looking to track daily expenses, set up monthly budgets, or monitor your financial progress over time, our tool 
+            offers the features you need to stay on top of your finances.
+            </p>
+          </div>
+          <div className='lg:w-[50%] w-full text-[18px] text-[#062134] font-semibold'>
+            <p>
+              Join thousands of satisfied users who have taken control of their finances with Expense Manager. Let us help you on your 
+              journey to financial wellness.
+            </p>
+          </div>
+        </div>
+        <div className='w-full pt-20 flex'>
+          <div className='w-[50%] flex items-center justify-center'>
+            <Image 
+            src=''
+            width={300}
+            height={300}
+            alt=''
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/app/components/heropage.jsx
+++ src/app/components/heropage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Heropage = () => {
   return (
-    <div className='w-full lg:bg-[url("/homepage.jpg")] bg-[url("/shopping.jpg")] lg:bg-auto bg-cover sm:bg-center lg:h-screen h-[80vh] lg:pt-20 lg:pl-20 p-5 leading-[50px]'>
+    <div className='w-full bg-[url("/shopping.jpg")] lg:bg-cover sm:bg-center lg:h-screen h-[80vh] lg:pt-20 lg:pl-20 p-5 leading-[50px]'>
         <h1 className='font-bold lg:text-[50px] text-[35px] lg:text-white text-[#062134]'>Take control of your finances</h1>
         <h1 className='font-bold lg:text-[50px] text-[35px] text-white'>with expense manager</h1>
         <p className='text-[20px] text-[#062134] typewriter-text font-semibold'>Effortlessly track your expenses, manage budgets, and achieve your financial goals.</p>
